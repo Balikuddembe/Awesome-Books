781e61d2aac89beba06a458f6614355a79673b94
Marcos Gualtero Lourenzo <marcosgualtero43253@gmail.com>
2022-09-27 19:45:32 -0300

    add refactor-classes changes

--- app.js
+++ app.js
@@ -22,68 +22,69 @@ class Storage {
     return books;
   }
 
-  static addBook () {
-    
+  static addBook (book) {
+    const books = Storage.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
   }
+
+  static removeBook(book) {
+    const bookTitle = book.querySelector('#tittle').innerText;
+    const books = Storage.getBooks();
+    const filterBooks = books.filter((book) => bookTitle === book.title);
+    const filterIndex = books.indexOf(filterBooks[0]);
+    books.splice(filterIndex, 1);
+    localStorage.setItem('books', JSON.stringify(books));
+  } 
 }
 
-const storedBooks = [];
-
-class dispayBooks {
-
-static addBooks(newBook) {
-  const bookStore = `<div class = "book">
-  <h2> ${newBook.title}</h2> 
-  <p class="by">by</p>
-  <h2> ${newBook.author}</h2>
-  <button class="remove" type="button">Remove</button>
-  <hr>
-  </div>`;
-  bookList.innerHTML += bookStore;
-  return bookList.innerHTML;
+class displayDynamic {
+    static displayBooks() {
+      const books = Storage.getBooks();
+      books.forEach((book) => displayDynamic.addBooksCollection(book));
+    }
+    static addBooksCollection(book) {
+        const books = document.querySelector('#list');
+        const newBook = document.createElement('div');
+        newBook.innerHTML = `
+        <div class = "book">
+        <h2> ${book.title}</h2> 
+        <p class="by">by</p>
+        <h2> ${book.author}</h2>
+        <button class="remove" type="button">Remove</button>
+        <hr>
+        </div>
+        
+        `;
+        newBook.classList.add('newBook');
+        books.appendChild(newBook);
+    }
+    static deleteBook(eve) {
+        if (eve.classList.contains('remove')) {
+            eve.parentElement.remove();
+        }
+    }
+    static clearFields() {
+        document.querySelector('#title').value = '';
+        document.querySelector('#author').value = '';
+    }
 }
 
-static addBook() {
-
-}
-
-static removeBook() {
-
-}
-// local storage section
-
-static getStorage() {
-  if (localStorage.localForm) {
-    localForm = JSON.parse(localStorage.localForm);
-    title.value = localForm.title;
-    author.value = localForm.author;
-  }
-}
-
-form.addEventListener('input', () => {
-  localStorage.localForm = JSON.stringify(localForm);
-  localForm.title = title.value;
-  localForm.author = author.value;
-});
-
-addBook.addEventListener('click', (e) => {
-  if (title.value === '' || author.value === '') {
+document.addEventListener('DOMContentLoaded', displayDynamic.displayBooks)
+document.querySelector('#form').addEventListener('click', (e) => {
     e.preventDefault();
-  } else {
-    const newBook = new Book(title.value, author.value);
-    addBooks(newBook);
-    title.value = '';
-    author.value = '';
-  }
-});
-// remove books section
+    const inputTitle = document.querySelector('#title').value;
+    const inputAuthor = document.querySelector('#author').value;
+    const book = new Book(inputTitle, inputAuthor);
+    displayDynamic.addBooksCollection(book);
+    Storage.addBook(book);
+    displayDynamic.clearFields();
+})
 
-bookList.addEventListener('click', (eve) => {
-  if (eve.target.classList.contains('remove')) {
-    document.querySelector('.list').removeChild(eve.target.parentElement);
-    const parent = eve.target.parentElement;
-    const removeBook = storedBooks.find((item) => item.title === parent.firstChild.innerText);
-    storedBooks.splice(storedBooks.indexOf(removeBook), 1);
-  }
-});
-}
+ document.querySelector('#list').addEventListener('clcik', (e) => {
+    if (e.target.className === 'remove') {
+       const book = e.target.parentElement;
+       Storage.removeBook(book);
+       displayDynamic.deleteBook(e.target);
+    }
+ });
