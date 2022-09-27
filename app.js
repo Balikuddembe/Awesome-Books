42a38d1b1999c7a8fed4a6a91ebcf0f1adc55a83
Emmanuel Balikuddembe <balixemma@gmail.com>
2022-09-27 18:13:30 +0200

    Fixed review comments

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const storedBooks = [];
 function addBooks(newBook) {
   const bookStore = `<div class = "book">
   <h2> ${newBook.title}</h2> 
+  <p class="by">by</p>
   <h2> ${newBook.author}</h2>
   <button class="remove" type="button">Remove</button>
   <hr>
@@ -18,35 +19,34 @@ function addBooks(newBook) {
   bookList.innerHTML += bookStore;
   return bookList.innerHTML;
 }
-addBook.addEventListener('click', (e) => {
-  const newBook = new Book(title.value, author.value);
-  if (title.value === '' || author.value === '') {
-    e.preventDefault();
-  } else {
-    addBooks(newBook);
-    title.value = '';
-    author.value = '';
-  }
-});
-
 // local storage section
 let localForm = { title: '', author: '' };
 if (localStorage.localForm) {
   localForm = JSON.parse(localStorage.localForm);
   title.value = localForm.title;
   author.value = localForm.author;
-} form.addEventListener('input', () => {
+} 
+form.addEventListener('input', () => {
   localStorage.localForm = JSON.stringify(localForm);
   localForm.title = title.value;
   localForm.author = author.value;
 });
 
+addBook.addEventListener('click', (e) => {
+  if (title.value === '' || author.value === '') {
+    e.preventDefault();
+  } else {
+    const newBook = new Book(title.value, author.value);
+    addBooks(newBook);
+    title.value = '';
+    author.value = '';
+  }
+});
 // remove books section
-
 bookList.addEventListener('click', (eve) => {
   if (eve.target.classList.contains('remove')) {
+    document.querySelector('.list').removeChild(eve.target.parentElement);
     const parent = eve.target.parentElement;
-    document.querySelector('.list').removeChild(parent);
     const removeBook = storedBooks.find((item) => item.title === parent.firstChild.innerText);
     storedBooks.splice(storedBooks.indexOf(removeBook), 1);
   }
