8b4d2c44b1eb0746d3fa03a884ecf4648ee04e38
Emmanuel Balikuddembe <balixemma@gmail.com>
2022-09-28 10:33:24 +0200

    Modified app.js

--- app.js
+++ app.js
@@ -1,90 +1,95 @@
-const addBook = document.querySelector('#btn');
-const bookList = document.querySelector('#list');
-const title = document.querySelector('#title');
-const author = document.querySelector('#author');
-const form = document.querySelector('#form');
+// const addBook = document.querySelector('#btn');
+// const bookList = document.querySelector('#list');
+// const title = document.querySelector('#title');
+// const author = document.querySelector('#author');
+// const form = document.querySelector('#form');
 
 class Book {
-  constructor (title,author) {
+  constructor(title, author) {
     this.title = title;
     this.author = author;
   }
-};
+}
 
 class Storage {
-  static getBooks(){
+  static getBooks() {
     let books;
-    if(localStorage.getItem('books')===null) {
-      books=[];
+    if (localStorage.getItem('books') === null) {
+      books = [];
     } else {
-      books = JSON.parse(localStorage.getItem('books'))
+      books = JSON.parse(localStorage.getItem('books'));
     }
     return books;
   }
 
-  static addBook (book) {
+  static addBook(book) {
     const books = Storage.getBooks();
     books.push(book);
     localStorage.setItem('books', JSON.stringify(books));
   }
 
   static removeBook(book) {
-    const bookTitle = book.querySelector('#tittle').innerText;
+    const bookTitle = book.querySelector('#title').innerText;
     const books = Storage.getBooks();
     const filterBooks = books.filter((book) => bookTitle === book.title);
     const filterIndex = books.indexOf(filterBooks[0]);
     books.splice(filterIndex, 1);
     localStorage.setItem('books', JSON.stringify(books));
-  } 
+  }
 }
 
 class displayDynamic {
-    static displayBooks() {
-      const books = Storage.getBooks();
-      books.forEach((book) => displayDynamic.addBooksCollection(book));
-    }
-    static addBooksCollection(book) {
-        const books = document.querySelector('#list');
-        const newBook = document.createElement('div');
-        newBook.innerHTML = `
-        <div class = "book">
-        <h2> ${book.title}</h2> 
+  static displayBooks() {
+    const books = Storage.getBooks();
+    books.forEach((book) => displayDynamic.addBooksCollection(book));
+  }
+
+  static addBooksCollection(book) {
+    const books = document.querySelector('.list');
+    const newBook = document.createElement('div');
+    newBook.innerHTML = `
+        <div>
+        <div class = "book-details">
+        <p id='title'> "${book.title}"</p> 
         <p class="by">by</p>
-        <h2> ${book.author}</h2>
-        <button class="remove" type="button">Remove</button>
-        <hr>
+        <p id='author'> ${book.author}</p>
         </div>
-        
+        <button class="remove" type="button">Remove</button>
+        </div>
+        <hr class='hr>
+        </div>
         `;
-        newBook.classList.add('newBook');
-        books.appendChild(newBook);
+    newBook.classList.add('newBook');
+    books.appendChild(newBook);
+  }
+
+  static deleteBook(eve) {
+    if (eve.classList.contains('remove')) {
+      eve.parentElement.parentElement.remove();
     }
-    static deleteBook(eve) {
-        if (eve.classList.contains('remove')) {
-            eve.parentElement.remove();
-        }
-    }
-    static clearFields() {
-        document.querySelector('#title').value = '';
-        document.querySelector('#author').value = '';
-    }
+  }
+
+  static clearFields() {
+    document.querySelector('.title').value = '';
+    document.querySelector('.author').value = '';
+  }
 }
 
-document.addEventListener('DOMContentLoaded', displayDynamic.displayBooks)
-document.querySelector('#form').addEventListener('click', (e) => {
-    e.preventDefault();
-    const inputTitle = document.querySelector('#title').value;
-    const inputAuthor = document.querySelector('#author').value;
-    const book = new Book(inputTitle, inputAuthor);
-    displayDynamic.addBooksCollection(book);
-    Storage.addBook(book);
-    displayDynamic.clearFields();
-})
+document.addEventListener('DOMContentLoaded', displayDynamic.displayBooks);
+document.querySelector('#form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const inputTitle = document.querySelector('.title').value;
+  const inputAuthor = document.querySelector('.author').value;
+  const book = new Book(inputTitle, inputAuthor);
+  displayDynamic.addBooksCollection(book);
+  Storage.addBook(book);
+  displayDynamic.clearFields();
+});
 
- document.querySelector('#list').addEventListener('clcik', (e) => {
-    if (e.target.className === 'remove') {
-       const book = e.target.parentElement;
-       Storage.removeBook(book);
-       displayDynamic.deleteBook(e.target);
-    }
- });
+document.querySelector('#list').addEventListener('click', (e) => {
+  if (e.target.className === 'remove') {
+    const book = e.target.parentElement;
+    Storage.removeBook(book);
+    displayDynamic.deleteBook(e.target);
+  }
+});
